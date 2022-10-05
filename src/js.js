e9ab8ce1eb531698678d0156ff2b1bd2aa8683ea
rimanai <109399022+rimanai@users.noreply.github.com>
2022-10-05 13:28:47 +1100

    Polish HTML page

--- src/js.js
+++ src/js.js
@@ -1,1 +1,16 @@
+let button = document.querySelector("#btn");
+button.addEventListener("click", handleClick);
 
+function handleClick() {
+  let vitc = document.querySelector("#firstCheckboxStretched");
+  vitc.addEventListener("change", (e) => {
+    if (e.checked) {
+      alert("You're using vitamin C");
+    } else {
+      alert("Oopsie");
+    }
+  });
+}
+
+//let subheading = document.querySelector("#sub");
+//console.log(subheading.textContent);
